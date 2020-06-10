d2135050ab293975b4c4d758b7eee9f12341b66b
Raghul0501 <66558082+Raghul0501@users.noreply.github.com>
2020-06-10 18:08:47 +0530

    Update posout.py

--- posout.py
+++ posout.py
@@ -1,11 +1,10 @@
 n=int(input("Enter number of elements:"))
 l=[]
+l1=[]
 for i in range(n):
     l.append(int(input(f"Enter element{i+1}:")))
 print(f"Input:{l}")
 for i in l:
-    if i < 0:
-        l.remove(i)
-print(f"Output:{l}")
-
-
+    if i > 0:
+        l1.append(i)
+print(f"Output:{l1}")
